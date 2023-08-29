0acd3e51ecd2d903cc5e4336044bdd7c6b87419d
Dibyendu Saha <dip4012@gmail.com>
2023-08-29 11:32:43 +0530

    implemented try catch

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const shortid = require("shortid");
-const Razorpay = require("razorpay");
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const shortid = require("shortid")
+const Razorpay = require("razorpay")
 // API
 
 // App Config
-const app = express();
+const app = express()
 
 const razorpay = new Razorpay({
 	key_id: "rzp_test_jsTm2eAq8BUZKf",
 	key_secret: "RjkUnbPfrW1poiMJo7AkT5oR",
-});
+})
 
 // Middlewares
-app.use(bodyParser.json());
-app.use(cors({origin: '*'}));
-app.use(express.json());
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json())
 
 // API routes
-app.get("/", (req, res) => res.status(200).send("Hello World"));
+app.get("/", (req, res) => res.status(200).send("Hello World"))
 
 app.post("/razorpay/create", async (req, res) => {
-	const response = await razorpay.orders.create({
-		amount: req.query.total,
-		currency: "INR",
-		receipt: shortid.generate(),
-		payment_capture: 1,
-	});
+	console.log("hii from /razorpay/create")
+	try {
+		const response = await razorpay.orders.create({
+			amount: req.query.total,
+			currency: "INR",
+			receipt: shortid.generate(),
+			payment_capture: 1,
+		})
 
-	res.json(response);
-});
+		res.json(response)
+	} catch (error) {
+		console.log(error)
+	}
+})
 
 app.post("/verification", (req, res) => {
-	const secret = "123456789";
+	try {
+		const secret = "123456789"
 
-	const crypto = require("crypto");
+		const crypto = require("crypto")
 
-	const shasum = crypto.createHmac("sha256", secret);
-	shasum.update(JSON.stringify(req.body));
-	const digest = shasum.digest("hex");
+		const shasum = crypto.createHmac("sha256", secret)
+		shasum.update(JSON.stringify(req.body))
+		const digest = shasum.digest("hex")
 
-	if (digest === req.headers["x-razorpay-signature"]) {
-		console.log("Request is legit");
-	} else {
+		if (digest === req.headers["x-razorpay-signature"]) {
+			console.log("Request is legit")
+		} else {
+		}
+		res.json({ status: "ok" })
+	} catch (error) {
+		console.log(error)
 	}
-	res.json({ status: "ok" });
-});
+})
 
 app.get("/payment/:id", async (req, res) => {
-	const payment_id = req.params.id;
-	const payment = await razorpay.payments.fetch(payment_id);
-	res.json(payment);
-});
+	try {
+		const payment_id = req.params.id
+		const payment = await razorpay.payments.fetch(payment_id)
+		res.json(payment)
+	} catch (error) {
+		console.log(error)
+	}
+})
 
 // Listen command
-app.listen(process.env.PORT || 1337, console.log("Server connected"));
+app.listen(process.env.PORT || 1337, console.log("Server connected"))
